ff029d5c2cdb65528d9d3113f73c0dc3cb3e370b
Pavel Kupreichik <91418557+kupreichikpavel@users.noreply.github.com>
2026-08-06 13:49:45 +0300

    feat: create order database schema

---
+++ src/main/java/by/innowise/orderservice/entity/Order.java
@@ -0,0 +1,70 @@
+package by.innowise.orderservice.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order extends AuditableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            name = "user_id",
+            nullable = false
+    )
+    private Long userId;
+
+    @Column(
+            name = "status",
+            nullable = false,
+            length = 50
+    )
+    private String status;
+
+    @Builder.Default
+    @Column(
+            name = "total_price",
+            nullable = false,
+            precision = 19,
+            scale = 2
+    )
+    private BigDecimal totalPrice = BigDecimal.ZERO;
+
+    @Builder.Default
+    @Column(
+            name = "deleted",
+            nullable = false
+    )
+    private boolean deleted = false;
+
+    @Builder.Default
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<OrderItem> orderItems = new ArrayList<>();
+}
